b2d0539374f9eb2ad90d798098dfc508a4450848
Latzke, Travis-CW <Travis.Latzke@bestbuy.com>
2020-01-06 09:56:00 -0600

    Updated the x,y gradient methods

--- test.py
+++ test.py
@@ -5,41 +5,49 @@ import numpy as np
 # One pitfall was figuring out how to calculate the x and y gradients
 
 
-def x_gradient(image):
-    dx = np.copy(image)
+def x_gradient_magnitudes(image):
+    height, width, channels = image.shape
+    # dx = np.copy(image)
+    dx = np.zeros((height, width))
 
-    for i in range(1, len(image) - 1):
-        for j in range(len(image[0])):
-            temp = dx[i+1][j] - dx[i-1][j]
-            # abs(dy[i][j+1] - dy[i][j-1])
-            dx[i][j] = temp
+    for i in range(height):
+        for j in range(width):
+            if i == 0 or i == height - 1:
+                dx[i][j] = np.linalg.norm(image[i][j])
+            else:
+                temp = image[i+1][j] - image[i-1][j]
+                dx[i][j] = np.sum(temp * temp)
 
     return dx
 
 
-def y_gradient(image):
-    dy = np.copy(image)
+def y_gradient_magnitudes(image):
+    height, width, channels = image.shape
+    # dy = np.copy(image)
+    dy = np.zeros((height, width))
 
-    for i in range(len(image)):
-        for j in range(1, len(image[0]) - 1):
-            temp = dy[i][j+1] - dy[i][j-1]
-            # abs(dy[i][j+1] - dy[i][j-1])
-            dy[i][j] = temp
+    for i in range(height):
+        for j in range(width):
+            if j == 0 or j == width - 1:
+                dy[i][j] = np.linalg.norm(image[i][j])
+            else:
+                temp = image[i][j+1] - image[i][j-1]
+                dy[i][j] = np.sum(temp * temp)
 
     return dy
 
 
-def gradient_magnitude(image):
-
-    height, width, _ = image.shape
-
-    delta = np.zeros((height, width))
-
-    for i in range(len(image)):
-        for j in range(len(image[0])):
-            delta[i][j] = np.sum(image[i][j] * image[i][j])
-
-    return delta
+# def gradient_magnitude(image):
+#
+#     height, width, _ = image.shape
+#
+#     delta = np.zeros((height, width))
+#
+#     for i in range(len(image)):
+#         for j in range(len(image[0])):
+#             delta[i][j] = np.sum(image[i][j] * image[i][j])
+#
+#     return delta
 
 
 def calculate_y_path_map(energy_map):
@@ -76,32 +84,33 @@ def calculate_x_path_map(energy_map):
 
 def remove_vertical_seam(image, y_map):
     height, width, _ = image.shape
-    index_to_be_removed_list = []
     to_be_removed_index = 0
     to_be_removed = np.zeros(height * 3)
+    choices = np.zeros(3, dtype=int)
 
     i = height - 1
     j = np.argmin(y_map[i, :])
 
     start_index = 3 * width * i + j * 3
     to_be_removed[to_be_removed_index] = start_index
-    index_to_be_removed_list.append(start_index)
-    index_to_be_removed_list.append(start_index + 1)
-    index_to_be_removed_list.append(start_index + 2)
+    to_be_removed[to_be_removed_index + 1] = start_index + 1
+    to_be_removed[to_be_removed_index + 2] = start_index + 2
+    to_be_removed_index += 3
 
     while i > 0:
         i = i - 1
-        left = np.max([j - 1, 0])
-        right = np.min([j + 1, width - 1])
-        choices = [left, j, right]
-        arg = np.argmin([y_map[i][left], y_map[i][j], y_map[i][right]])
-        j = choices[arg]
+        choices[0] = np.max([j - 1, 0])
+        choices[1] = j
+        choices[2] = np.min([j + 1, width - 1])
+        a_min = np.argmin([y_map[i][choices[0]], y_map[i][j], y_map[i][choices[2]]])
+        j = choices[a_min]
         start_index = 3 * width * i + j * 3
-        index_to_be_removed_list.append(start_index)
-        index_to_be_removed_list.append(start_index + 1)
-        index_to_be_removed_list.append(start_index + 2)
+        to_be_removed[to_be_removed_index] = start_index
+        to_be_removed[to_be_removed_index + 1] = start_index + 1
+        to_be_removed[to_be_removed_index + 2] = start_index + 2
+        to_be_removed_index += 3
 
-    intermediate_image = np.delete(image, index_to_be_removed_list, None)
+    intermediate_image = np.delete(image, to_be_removed, None)
 
     new_image = intermediate_image.reshape((height, width - 1, 3))
 
@@ -110,23 +119,25 @@ def remove_vertical_seam(image, y_map):
 
 def scale_image(image_to_scale):
 
-    for i in range(1):
+    # 103
+    for i in range(71):
         print(i)
-        dx = x_gradient(image_to_scale)
-        dy = y_gradient(image_to_scale)
-        dI = dx + dy
-
-        energy_map = gradient_magnitude(dI)
+        dx = x_gradient_magnitudes(image_to_scale)
+        dy = y_gradient_magnitudes(image_to_scale)
+        # dI = dx + dy
+        energy_map = dx + dy
+        # energy_map = gradient_magnitude(dI)
         y_map = calculate_y_path_map(energy_map)
         image_to_scale = remove_vertical_seam(image_to_scale, y_map)
         image_new_image = Image.fromarray(image_to_scale)
-        image_new_image.save("island_" + str(i) + ".png")
+        image_new_image.save("pics/islands_" + str(i) + ".png")
 
     return image_to_scale
 
 
+# choices = np.zeros(3, dtype=int)
 # Read image
-img = Image.open('island_original.png')
+img = Image.open('islands_inter_2.png')
 
 # img.save('island_original_copy.png')
 
