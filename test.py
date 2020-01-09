6f52fefb9ae95e88a41584a4b0420e1063ae916d
Latzke, Travis-CW <Travis.Latzke@bestbuy.com>
2020-01-09 17:11:32 -0600

    Added more robust code to seam removal

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 import time
+import cv2
 
 # One pitfall was figuring out how to calculate the x and y gradients
 
@@ -51,19 +52,6 @@ def y_gradient_magnitudes(image):
     return dy
 
 
-# def gradient_magnitude(image):
-#
-#     height, width, _ = image.shape
-#
-#     delta = np.zeros((height, width))
-#
-#     for i in range(len(image)):
-#         for j in range(len(image[0])):
-#             delta[i][j] = np.sum(image[i][j] * image[i][j])
-#
-#     return delta
-
-
 def calculate_y_path_map(energy_map):
     height, width = energy_map.shape
     y_map = np.zeros((height, width))
@@ -110,24 +98,6 @@ def calculate_y_path_map(energy_map):
         np.put(y_map[i], not_and_i, y_map[i-1][not_and_i])
         y_map[i] = y_map[i] + energy_map[i]
 
-    # choices = np.zeros(3)
-    # old_y_map = np.zeros((height, width))
-
-    # for i in range(height):
-    #     for j in range(width):
-    #         if i == 0:
-    #             old_y_map[i][j] = energy_map[i][j]
-    #         else:
-    #             left = np.max([j - 1, 0])
-    #             right = np.min([j + 1, width - 1])
-    #             choices[0] = old_y_map[i-1][left]
-    #             choices[1] = old_y_map[i-1][j]
-    #             choices[2] = old_y_map[i-1][right]
-    #             old_y_map[i][j] = energy_map[i][j] + np.min(choices)
-    #
-    #         if old_y_map[i][j] != y_map[i][j]:
-    #             print("wrong")
-
     return y_map
 
 
@@ -147,39 +117,83 @@ def calculate_x_path_map(energy_map):
     return x_map
 
 
-def remove_vertical_seam(image, y_map):
+def calculate_energy(image):
+    b_0 = image[:,:,0]
+    c_0 = image[:,:,0]
+    d_0 = image[:,:,0]
+
+    b_energy_0 = np.absolute(cv2.Scharr(b_0, -1, 1, 0)) + np.absolute(cv2.Scharr(b_0, -1, 0, 1))
+    c_energy_0 = np.absolute(cv2.Scharr(c_0, -1, 1, 0)) + np.absolute(cv2.Scharr(c_0, -1, 0, 1))
+    d_energy_0 = np.absolute(cv2.Scharr(d_0, -1, 1, 0)) + np.absolute(cv2.Scharr(d_0, -1, 0, 1))
+
+    return b_energy_0 + c_energy_0 + d_energy_0
+
+
+def delete_seam(image, seam_idx):
     height, width, _ = image.shape
-    to_be_removed_index = 0
-    to_be_removed = np.zeros(height * 3)
-    choices = np.zeros(3, dtype=int)
+    output = np.zeros((height, width - 1, 3), dtype='uint8')
 
+    for row in range(height):
+        col = seam_idx[row]
+        output[row, :, 0] = np.delete(image[row, :, 0], [col])
+        output[row, :, 1] = np.delete(image[row, :, 1], [col])
+        output[row, :, 2] = np.delete(image[row, :, 2], [col])
+
+    return output
+
+
+def calculate_seam(image, y_map):
+    height, width, _ = image.shape
+    seam = np.zeros(height, dtype=int)
     i = height - 1
-    j = np.argmin(y_map[i, :])
-
-    start_index = 3 * width * i + j * 3
-    to_be_removed[to_be_removed_index] = start_index
-    to_be_removed[to_be_removed_index + 1] = start_index + 1
-    to_be_removed[to_be_removed_index + 2] = start_index + 2
-    to_be_removed_index += 3
+    seam[i] = np.argmin(y_map[i, :])
+    choices = np.zeros(3, dtype=int)
 
     while i > 0:
         i = i - 1
+        j = seam[i + 1]
         choices[0] = np.max([j - 1, 0])
         choices[1] = j
         choices[2] = np.min([j + 1, width - 1])
         a_min = np.argmin([y_map[i][choices[0]], y_map[i][j], y_map[i][choices[2]]])
-        j = choices[a_min]
-        start_index = 3 * width * i + j * 3
-        to_be_removed[to_be_removed_index] = start_index
-        to_be_removed[to_be_removed_index + 1] = start_index + 1
-        to_be_removed[to_be_removed_index + 2] = start_index + 2
-        to_be_removed_index += 3
+        seam[i] = choices[a_min]
 
-    intermediate_image = np.delete(image, to_be_removed, None)
+    return seam
 
-    new_image = intermediate_image.reshape((height, width - 1, 3))
 
-    return new_image
+# def remove_vertical_seam(image, y_map):
+#     height, width, _ = image.shape
+#     to_be_removed_index = 0
+#     to_be_removed = np.zeros(height * 3)
+#     choices = np.zeros(3, dtype=int)
+#
+#     i = height - 1
+#     j = np.argmin(y_map[i, :])
+#
+#     start_index = 3 * width * i + j * 3
+#     to_be_removed[to_be_removed_index] = start_index
+#     to_be_removed[to_be_removed_index + 1] = start_index + 1
+#     to_be_removed[to_be_removed_index + 2] = start_index + 2
+#     to_be_removed_index += 3
+#
+#     while i > 0:
+#         i = i - 1
+#         choices[0] = np.max([j - 1, 0])
+#         choices[1] = j
+#         choices[2] = np.min([j + 1, width - 1])
+#         a_min = np.argmin([y_map[i][choices[0]], y_map[i][j], y_map[i][choices[2]]])
+#         j = choices[a_min]
+#         start_index = 3 * width * i + j * 3
+#         to_be_removed[to_be_removed_index] = start_index
+#         to_be_removed[to_be_removed_index + 1] = start_index + 1
+#         to_be_removed[to_be_removed_index + 2] = start_index + 2
+#         to_be_removed_index += 3
+#
+#     intermediate_image = np.delete(image, to_be_removed, None)
+#
+#     new_image = intermediate_image.reshape((height, width - 1, 3))
+#
+#     return new_image
 
 
 def scale_image(image_to_scale):
@@ -200,13 +214,17 @@ def scale_image(image_to_scale):
         dy_time += time.time() - start_time
         # dI = dx + dy
         energy_map = dx + dy
+
+        # energy_map = calculate_energy(image_to_scale)
         # energy_map = gradient_magnitude(dI)
         start_time = time.time()
         y_map = calculate_y_path_map(energy_map)
         path_time += time.time() - start_time
 
         start_time = time.time()
-        image_to_scale = remove_vertical_seam(image_to_scale, y_map)
+        seam = calculate_seam(image_to_scale, y_map)
+        image_to_scale = delete_seam(image_to_scale, seam)
+        # image_to_scale = remove_vertical_seam(image_to_scale, y_map)
         removal_time += time.time() - start_time
 
         if i % 10 == 0:
@@ -217,38 +235,16 @@ def scale_image(image_to_scale):
     return image_to_scale
 
 
-# choices = np.zeros(3, dtype=int)
-# Read image
 img = Image.open('island_original.png')
-
-# img.save('island_original_copy.png')
 
 img_array = np.asarray(img)
 
-# prints format of image
 print(img.format)
 
-# dx = x_gradient(img_array)choices
-# dy = y_gradient(img_array)
-#
-# dI = dx + dy
-# energy_map = gradient_magnitude(dI)
-#
-# y_map = calculate_y_path_map(energy_map)
-#
-# new_image = remove_vertical_seam(img_array, y_map)
 new_image = scale_image(img_array)
 
-# image_dx = Image.fromarray(dx)
-# image_dy = Image.fromarray(dy)
-# image_dI = Image.fromarray(dI)
-# image_m = Image.fromarray(energy_map, 'L')
 image_new_image = Image.fromarray(new_image)
 
 image_new_image.show()
-# image_dx.show()
-# image_dy.show()
-# image_dI.show()
-# image_m.show()
 
 print()
