8e9fa09bbf4731b4a78cb95fc3d7d2bf9ca0d74f
Latzke, Travis-CW <Travis.Latzke@bestbuy.com>
2020-02-01 19:59:46 -0600

    Blending code after attempt 1

--- pyramid/blending.py
+++ pyramid/blending.py
@@ -51,62 +51,6 @@ def ceiling(number):
     return number_to_int + 1
 
 
-# def reduce_channel(image_channel_current, h_next, w_next):
-#     w = np.array([
-#         [1, 4, 6, 4, 1],
-#         [4, 16, 24, 16, 4],
-#         [6, 24, 36, 24, 6],
-#         [4, 16, 24, 16, 4],
-#         [1, 4, 6, 4, 1],
-#     ]) / 256
-
-    # dst = cv2.copyMakeBorder(image_channel_current, 2, 2, 2, 2, cv2.BORDER_REPLICATE, None)
-    #
-    # M = 5
-    # N = 5
-    #
-    # image_channel_1 = np.zeros((h_next, w_next), dtype="uint8")
-    #
-    # # this implementation is based off of the formula from http://persci.mit.edu/pub_pdfs/spline83.pdf page 222.
-    # for i in range(h_next):
-    #     for j in range(w_next):
-    #         temp = 0
-    #         for m in range(M):
-    #             for n in range(N):
-    #                 temp += dst[2 * i + m][2 * j + n] * w[m][n]
-    #                 # handle overflow
-    #         if temp > 255:
-    #             temp = 255
-    #         image_channel_1[i][j] = int(temp)
-    #
-    # return image_channel_1
-
-
-# def expand_channel(image_channel, h_next, w_next):
-#
-#     channel_next = np.zeros((h_next, w_next), dtype="uint8")
-#
-#     dst = cv2.copyMakeBorder(image_channel, 2, 2, 2, 2, cv2.BORDER_REPLICATE, None)
-#
-#     for i in range(h_next):
-#         for j in range(w_next):
-#             summation = 0
-#             for m in range(-2, 3):
-#                 for n in range(-2, 3):
-#                     pixeli = (i - m)/2
-#                     pixelj = (j - n)/2
-#                     if (int(pixeli) == pixeli) and (int(pixelj) == pixelj):
-#                         pixeli = pixeli + 2
-#                         pixelj = pixelj + 2
-#                         tmpval =  dst[int(pixeli)][int(pixelj)] * w[m + 2][n + 2]
-#                         summation += tmpval
-#             if summation > 255:
-#                 summation = 255
-#             channel_next[i][j] = 4 * summation
-#
-#     return channel_next
-
-
 def generatingKernel(a):
     """Return a 5x5 generating kernel based on an input parameter (i.e., a
     square "5-tap" filter.)
@@ -167,57 +111,18 @@ def reduce_layer(image, kernel=generatingKernel(0.4)):
         An image of shape (ceil(r/2), ceil(c/2)). For instance, if the input is
         5x7, the output will be 3x4.
     """
-    # dst = cv2.copyMakeBorder(image, 2, 2, 2, 2, cv2.BORDER_REFLECT101, None)
     h, w = image.shape
     h_new = ceiling(h / 2)
     w_new = ceiling(w / 2)
 
-    blurred = cv2.filter2D(image, -1, kernel=kernel, borderType=cv2.BORDER_REFLECT101) # np.convolve(dst, kernel)
+    flipped_kernel = np.flip(kernel)
+    blurred = cv2.filter2D(image, -1, kernel=flipped_kernel, borderType=cv2.BORDER_REFLECT101)
     image_new = np.zeros((h_new, w_new))
 
     for i in range(h_new):
         image_new[i][:] = blurred[i*2][0:w + 1: 2]
 
-    return image_new.astype("uint8")
-
-    # M = 5
-    # N = 5
-
-
-
-    # new_image = np.zeros((h, w), dtype="uint8")
-    #
-    # # this implementation is based off of the formula from http://persci.mit.edu/pub_pdfs/spline83.pdf page 222.
-    # for i in range(h_new):
-    #     for j in range(w_new):
-    #         temp = 0
-    #         for m in range(M):
-    #             for n in range(N):
-    #                 temp += dst[2 * i + m][2 * j + n] * kernel[m][n]
-    #                 # handle overflow
-    #         if temp > 255:
-    #             temp = 255
-    #         new_image[i][j] = int(temp)
-
-    # return new_image
-
-    # WRITE YOUR CODE HERE.
-    # h_current, w_current, _ = image.shape
-    # h_prev = ceiling(h_current / 2)
-    # w_prev = ceiling(w_current / 2)
-
-    # g_next = np.zeros((h_prev, w_prev))
-    #
-    # channel_0 = reduce_channel(image[:, :, 0], h_prev, w_prev)
-    # channel_1 = reduce_channel(image[:, :, 1], h_prev, w_prev)
-    # channel_2 = reduce_channel(image[:, :, 2], h_prev, w_prev)
-    #
-    # g_next[:, :, 0] = channel_0
-    # g_next[:, :, 1] = channel_1
-    # g_next[:, :, 2] = channel_2
-    # g_next = cv2.pyrDown(image)
-    # return g_next
-    # raise NotImplementedError
+    return normalize(image_new)
 
 
 def expand_layer(image, kernel=generatingKernel(0.4)):
@@ -276,34 +181,16 @@ def expand_layer(image, kernel=generatingKernel(0.4)):
     for i in range(h):
         image_new[i*2][0:w*2 + 1:2] = image[i]
 
-    blurred = cv2.filter2D(image, -1, kernel=kernel, borderType=cv2.BORDER_REFLECT101)
+    flipped_kernel = np.flip(kernel)
+    blurred = 4 * cv2.filter2D(image_new, -1, kernel=flipped_kernel, borderType=cv2.BORDER_REFLECT101)
 
-    return blurred
+    return normalize(blurred)
 
-    # WRITE YOUR CODE HERE.
-    # h, w = image.shape
-    # channel_next = np.zeros((h, w), dtype="uint8")
-    #
-    # dst = cv2.copyMakeBorder(image, 2, 2, 2, 2, cv2.BORDER_REFLECT101, None)
 
-    # for i in range(h):
-    #     for j in range(w):
-    #         summation = 0
-    #         for m in range(-2, 3):
-    #             for n in range(-2, 3):
-    #                 pixeli = (i - m)/2
-    #                 pixelj = (j - n)/2
-    #                 if (int(pixeli) == pixeli) and (int(pixelj) == pixelj):
-    #                     pixeli = pixeli + 2
-    #                     pixelj = pixelj + 2
-    #                     tmpval =  dst[int(pixeli)][int(pixelj)] * kernel[m + 2][n + 2]
-    #                     summation += tmpval
-    #         if summation > 255:
-    #             summation = 255
-    #         channel_next[i][j] = 4 * summation
-
-    # return channel_next
-    # return g_next
+def normalize(image):
+    m = np.max(image)
+    normed = np.multiply(image, image) / (m*m)
+    return normed
 
 
 def gaussPyramid(image, levels):
@@ -338,12 +225,10 @@ def gaussPyramid(image, levels):
     layers_of_pyramid.append(image_to_reduce)
 
     for i in range(levels):
-        print('Reducing...')
         image_to_reduce = reduce_layer(image_to_reduce)
         layers_of_pyramid.append(image_to_reduce)
 
     return layers_of_pyramid
-    # raise NotImplementedError
 
 
 def laplPyramid(gaussPyr):
@@ -384,22 +269,20 @@ def laplPyramid(gaussPyr):
         result in an image of size 6x8. In this case, crop the expanded layer
         to 5x7.
     """
-    # WRITE YOUR CODE HERE.
     gaussPyr_length = len(gaussPyr)
 
     lapl_pyramid_layers = []
     for i in range(gaussPyr_length - 1):
-        print("Expanding")
         current = gaussPyr[i]
         next = gaussPyr[i + 1]
-        # expanded = expand_layer(next)
-        expanded = expand_layer(next) # cv2.pyrUp(next)
+        expanded = expand_layer(next)
 
         h, w = current.shape
         h_, w_ = expanded.shape
         if h != h_ or w != w_:
             expanded = cv2.resize(expanded, (w, h))
-        laplacian = cv2.subtract(current.astype("uint8"), expanded.astype("uint8"))
+        # cv2.subtract(current, expanded)
+        laplacian = np.subtract(current, expanded)
         lapl_pyramid_layers.append(laplacian)
 
     lapl_pyramid_layers.append(gaussPyr[gaussPyr_length - 1])
@@ -452,16 +335,16 @@ def blend(laplPyrWhite, laplPyrBlack, gaussPyrMask):
     number_of_layers = len(laplPyrWhite)
 
     for i in range(number_of_layers):
-        print("Blending layer: " + str(i))
         lapl_layer_i_white = laplPyrWhite[i]
         lapl_layer_i_black = laplPyrBlack[i]
         gauss_layer_i_mask = gaussPyrMask[i]
 
-        mask_inverse = 255 - gauss_layer_i_mask
-        new_lapl_white = np.multiply(lapl_layer_i_white, gauss_layer_i_mask / 255).astype("uint8")
-        new_lapl_black = np.multiply(lapl_layer_i_black, mask_inverse / 255).astype("uint8")
+        mask_inverse = 1 - gauss_layer_i_mask
 
-        blended_layer = cv2.add(new_lapl_white.astype("uint8"), new_lapl_black.astype("uint8"))
+        new_lapl_white = np.multiply(lapl_layer_i_white, gauss_layer_i_mask)
+        new_lapl_black = np.multiply(lapl_layer_i_black, mask_inverse)
+
+        blended_layer = cv2.add(new_lapl_white, new_lapl_black)
         blended_pyramed.append(blended_layer)
 
     return blended_pyramed
@@ -501,15 +384,14 @@ def collapse(pyramid):
     # WRITE YOUR CODE HERE.
     aggregated = pyramid[len(pyramid) - 1]
     for i in range(len(pyramid) - 2, -1, -1):
-        print(i)
         current = pyramid[i]
-        exp = expand_layer(aggregated.astype("uint8"))
-        # exp = cv2.pyrUp(aggregated.astype("uint8"))
+        exp = expand_layer(aggregated)
         h, w = current.shape
         h_, w_ = exp.shape
         if h != h_ or w != w_:
             exp = cv2.resize(exp, (w, h))
-        aggregated = cv2.add(current.astype("uint8"), exp.astype("uint8"))
+        aggregated = cv2.add(current, exp)
+        # aggregated = current + exp
 
     return aggregated
 
